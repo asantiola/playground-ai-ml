2d310a9341b11610b15a887c75f951051d97b247
asantiola <asantiola@gmail.com>
2025-09-15 11:57:12 +0800

    create a rag using redis as in memory store

---
+++ src/dmr-practice/lessons/rag01-redis-in_memory.py
@@ -0,0 +1,78 @@
+import os
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_community.document_loaders import TextLoader
+from langchain_redis import RedisConfig, RedisVectorStore
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+import redis
+
+embeddings_model = "thenlper/gte-small"
+
+doc_path = "/Users/asantiola/repo/playground-ai-ml/data/documents-txt"
+files = [os.path.join(doc_path, file) for file in os.listdir(doc_path)]
+docs_lists = [TextLoader(file).load_and_split() for file in files]
+
+hf_embeddings = HuggingFaceEmbeddings(
+    cache_folder="/Users/asantiola/repo/playground-ai-ml/.cache",
+    model_name=embeddings_model,
+    model_kwargs={"device": "cpu"},
+    encode_kwargs={"normalize_embeddings": False},
+)
+
+redis_url = "redis://default:password@localhost:6379"
+index_name = "vector_index"
+config = RedisConfig(
+    index_name=index_name,
+    redis_url=redis_url,
+)
+
+client = redis.from_url(redis_url)
+print("client.ping(): ", client.ping())
+
+client.flushall()
+
+vector_store = RedisVectorStore(
+    embeddings=hf_embeddings, 
+    config=config
+)
+
+for docs_list in docs_lists:
+    vector_store.add_documents(docs_list)
+
+llm = ChatOpenAI(
+    model="ai/llama3.1",
+    temperature=0,
+    base_url="http://localhost:12434/engines/v1",
+    api_key="docker",
+)
+
+prompt = PromptTemplate(
+    template="""You are an assistant for question-answering tasks. 
+        Use the following documents to answer the question. 
+
+        If you don't know the answer, just say that you don't know. 
+
+        Use three sentences maximum and keep the answer concise:
+        Question: {question} 
+        Documents: {documents} 
+        Answer: 
+        """,
+    input_variables=["question", "documents"],
+)
+
+rag_chain = prompt | llm
+retriever = vector_store.as_retriever()
+
+def query(question):
+    print(f"Question: \"{question}\"\n")
+    documents = retriever.invoke(question)
+    answer = rag_chain.invoke({
+        "documents": documents,
+        "question": question,
+    })
+    print(f"Answer: {answer.content}\n\n")
+
+questions = "/Users/asantiola/repo/playground-ai-ml/data/questions.txt"
+with open(questions) as file:
+    for line in file:
+        query(line.rstrip())
