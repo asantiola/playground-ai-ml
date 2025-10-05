2833cf9e98566c0b1838aa05942a06eb12f252b0
asantiola <asantiola@gmail.com>
2025-10-05 23:57:09 +0800

    update model path

--- src/dmr-practice/lessons/rag-routing/rag-routing-01.py
+++ src/dmr-practice/lessons/rag-routing/rag-routing-01.py
@@ -63,7 +63,7 @@ def select_retriever(question: str):
     return retriever_technologies
 
 llm = ChatOpenAI(
-    model="ai/gpt-oss",
+    model="ai/gpt-oss:latest",
     temperature=0,
     base_url="http://localhost:12434/engines/v1",
     api_key="docker",
--- src/dmr-practice/lessons/rag-routing/rag-routing-03.py
+++ src/dmr-practice/lessons/rag-routing/rag-routing-03.py
@@ -277,7 +277,7 @@ retriever_technologies = create_retriever(HOME + "/repo/playground-ai-ml/data/ro
 retrievers = (None, retriever_billiards, retriever_guitars, retriever_technologies)
 
 llm = ChatOpenAI(
-    model="ai/gpt-oss",
+    model="ai/gpt-oss:latest",
     temperature=0,
     base_url="http://localhost:12434/engines/v1",
     api_key="docker",
