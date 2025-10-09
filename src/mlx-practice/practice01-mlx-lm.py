4506a450aca620227ac90f24085bd91751af2eb3
asantiola <asantiola@gmail.com>
2025-10-09 10:10:40 +0800

    update practice files

--- src/mlx-practice/practice01-mlx-lm.py
+++ src/mlx-practice/practice01-mlx-lm.py
@@ -1,7 +1,7 @@
 from mlx_lm import load, generate
 
-# model_path = "mlx-community/gpt-oss-20b-MXFP4-Q4"
-model_path = "mlx-community/Meta-Llama-3.1-8B-Instruct-4bit"
+model_path = "mlx-community/gpt-oss-20b-MXFP4-Q4"
+# model_path = "mlx-community/Meta-Llama-3.1-8B-Instruct-4bit"
 model, tokenizer = load(model_path)
 
 chat_history = [
--- src/mlx-practice/practice03-langchain-openai.py
+++ src/mlx-practice/practice03-langchain-openai.py
@@ -1,10 +1,12 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 # practice code using langchain_openai.ChatOpenAI
 
 llm = ChatOpenAI(
-    model = "mlx-community/Meta-Llama-3.1-8B-Instruct-4bit",
-    # model="mlx-community/gpt-oss-20b-MXFP4-Q4",
+    # model = "mlx-community/Meta-Llama-3.1-8B-Instruct-4bit",
+    model="mlx-community/gpt-oss-20b-MXFP4-Q4",
     temperature=0,
     base_url="http://localhost:12434/v1",
     api_key="mlx-lm",
@@ -12,8 +14,13 @@ llm = ChatOpenAI(
 
 messages = [
     ("system", "You are a helpful AI assistant."),
-    ("human", "Create a short story about a chicken and a cat. Just create the story. No need to provide the analysis or explanation."),
+    ("human", "{input}"),
 ]
 
-ai_msg = llm.invoke(messages)
-print(ai_msg.content)
+prompt = ChatPromptTemplate.from_messages(messages)
+
+chain = (prompt | llm | StrOutputParser())
+
+input = "Create a short story about a chicken and a cat. Just create the story. No need to provide the analysis or explanation."
+output = chain.invoke(input)
+print(output)
