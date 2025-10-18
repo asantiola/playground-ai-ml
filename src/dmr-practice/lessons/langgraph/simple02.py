96df8a990c417799d073d9682e89d1d6df65981a
asantiola <asantiola@gmail.com>
2025-10-18 16:24:49 +0800

    add sample snippets

--- src/dmr-practice/lessons/langgraph/simple02.py
+++ src/dmr-practice/lessons/langgraph/simple02.py
@@ -1,5 +1,5 @@
 from typing import Annotated, TypedDict, List
-from langgraph.graph import StateGraph, END
+from langgraph.graph import StateGraph, START, END
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage
 from langgraph.checkpoint.memory import MemorySaver
@@ -25,16 +25,14 @@ def chatbot(state: State):
 
 graph_builder = StateGraph(State)
 graph_builder.add_node("chatbot", chatbot)
-graph_builder.set_entry_point("chatbot")
+# graph_builder.set_entry_point("chatbot") #alternative below
+graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
 
 memory = MemorySaver()
 app = graph_builder.compile(checkpointer=memory)
 
 config = {"configurable": {"thread_id": "my_conversation_1"}}
-
-# user_input = input("User: ")
-# input_message = HumanMessage(content=user_input)
 
 initial_messages = [
     SystemMessage(content="You are Mario from Super Mario Brothers that helps answer questions."),
@@ -45,19 +43,20 @@ result = app.invoke({ "messages": initial_messages }, config=config)
 print(f"result:\n{result}\n")
 print(f"answer:\n{result["messages"][-1].content}\n")
 
-input_message = HumanMessage(content="From which continent is it?")
-result = app.invoke({ "messages": [input_message] }, config=config)
+# # Get user input
+# user_input = input("User: ")
+# input_message = [HumanMessage(content=user_input)]
+
+input_message = [
+    HumanMessage(content="From which continent is it?")
+]
+result = app.invoke({ "messages": input_message }, config=config)
 print(f"result:\n{result}\n")
 print(f"answer:\n{result["messages"][-1].content}\n")
 
-input_message = HumanMessage(content="What are the countries around it?")
-result = app.invoke({ "messages": [input_message] }, config=config)
+input_message = [
+    HumanMessage(content="What are the countries around it?")
+]
+result = app.invoke({ "messages": input_message }, config=config)
 print(f"result:\n{result}\n")
 print(f"answer:\n{result["messages"][-1].content}\n")
-
-# result = app.invoke({}, result)
-# print(f"result2:\n{result}\n")
-
-# for event in app.stream({ "messages": [("user", user_input)]}):
-#     for value in event.values():
-#         print("Assistant:", value["messages"][-1].content)
