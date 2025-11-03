83f886c380499da88d23c8d7bfc21a6ad206adda
asantiola <asantiola@gmail.com>
2025-11-04 00:07:29 +0800

    update test code

--- src/mlx-practice/practice03-langchain-openai.py
+++ src/mlx-practice/practice03-langchain-openai.py
@@ -5,8 +5,8 @@ from langchain_core.output_parsers import StrOutputParser
 # practice code using langchain_openai.ChatOpenAI
 
 llm = ChatOpenAI(
-    # model = "mlx-community/Meta-Llama-3.1-8B-Instruct-4bit",
-    model="mlx-community/gpt-oss-20b-MXFP4-Q4",
+    model = "mlx-community/Meta-Llama-3.1-8B-Instruct-4bit",
+    # model="mlx-community/gpt-oss-20b-MXFP4-Q4",
     temperature=0,
     base_url="http://localhost:12434/v1",
     api_key="mlx-lm",
@@ -21,6 +21,6 @@ prompt = ChatPromptTemplate.from_messages(messages)
 
 chain = (prompt | llm | StrOutputParser())
 
-input = "Create a short story about a chicken and a cat. Just create the story. No need to provide the analysis or explanation."
+input = "Tell me a joke about cheese."
 output = chain.invoke(input)
 print(output)
