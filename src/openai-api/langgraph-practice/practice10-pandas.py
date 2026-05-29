4c0e2e7caec42bb32a4469135158ad758ef342ce
asantiola <asantiola@gmail.com>
2026-05-29 23:25:24 +0800

    add practice pandas ai analysis

---
+++ src/openai-api/langgraph-practice/practice10-pandas.py
@@ -0,0 +1,46 @@
+from operator import contains
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage
+import pandas as pd
+import json
+import os
+
+openai_base_url = os.environ.get(
+    "OPENAI_BASE_URL", 
+    "http://model-runner.docker.internal/engines/v1"
+)
+
+api_key = os.environ.get(
+    "OPENAI_API_KEY",
+    "your-default-key"
+)
+
+llm = ChatOpenAI(
+    model="ai/gemma4:E4B",
+    base_url=openai_base_url,
+    api_key=api_key,
+)
+
+
+HOME = os.environ["HOME"]
+financial_file = HOME + "/playground-ai-ml/data/financial01.json"
+
+try:
+    with open(financial_file, "r") as f:
+        financial_data = json.load(f)
+except FileNotFoundError:
+    print(f"Error: file not found: {financial_file}")
+    exit
+except json.JSONDecodeError:
+    print(f"Error: file {financial_file} is not a valid JSON")
+    exit
+
+df = pd.DataFrame(financial_data["market_data"])
+df_metadata = f"Columns: {list(df.columns)}\nData Types:\n{df.dtypes.to_string()}"
+
+system_prompt = f"You are an expert assistant good at analyzing pandas dataframes. Metadata is {df_metadata}"
+human_prompt = f"Given the dataset {df}, give the top 3 performing companies, and explain."
+
+messages = [SystemMessage(content=system_prompt), HumanMessage(content=human_prompt)]
+response = llm.invoke(messages)
+print(response.content)
