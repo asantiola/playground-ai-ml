a48292cd5be431dd69f191aa54782f4e5a48e3fd
asantiola <asantiola@gmail.com>
2026-06-13 00:36:09 +0800

    try wav file

--- src/openai-api/langgraph-practice/practice09-audio.py
+++ src/openai-api/langgraph-practice/practice09-audio.py
@@ -1,5 +1,5 @@
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 import base64
 import os
 
@@ -30,25 +30,36 @@ def encode_audio(audio_path):
         return base64.b64encode(image_file.read()).decode('utf-8')
 
 def describe(audio_path):
-    message = HumanMessage(
-        content=[
-            {
-                "type": "text",
-                "text": "Describe this audio file.",
-            },
-            {
-                "type": "input_audio",
-                "input_audio": {
-                    "data": encode_audio(audio_path),
-                    "format": "mp3", # OpenAI strictly requires "wav" or "mp3"
-                },
-            },
-        ]
+    system_prompt = (
+        "You are an expert audio analyst and acoustic engineer. Your task is to analyze "
+        "audio clips with precise attention to detail, covering speech content, vocal characteristics, "
+        "background environment, and audio quality."
     )
+    human_prompt = (
+        "What is being said in the audio file?"
+    )
+    messages = [
+        SystemMessage(content=system_prompt),
+        HumanMessage(
+            content=[
+                {
+                    "type": "text",
+                    "text": human_prompt,
+                },
+                {
+                    "type": "input_audio",
+                    "input_audio": {
+                        "data": encode_audio(audio_path),
+                        "format": "mp3", # OpenAI strictly requires "wav" or "mp3"
+                    },
+                },
+            ]
+        )
+    ]
 
-    response = llm.invoke([message])
+    response = llm.invoke(messages)
     print(f"\n===== AI RESPONSE =====\n{response.content}\n")
 
-path_quickbrownfox = workspaces + "/playground-ai-ml/data/audios/QuickBrownFox.mp3"
+path_quickbrownfox = workspaces + "/playground-ai-ml/data/audios/QuickBrownFox.wav"
 
 describe(path_quickbrownfox)
