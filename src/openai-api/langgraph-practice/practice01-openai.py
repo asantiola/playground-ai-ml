32be95b0782a8cab25845a176970fa6d18c67b2b
asantiola <asantiola@gmail.com>
2026-06-16 21:36:34 +0800

    fix typo

--- src/openai-api/langgraph-practice/practice01-openai.py
+++ src/openai-api/langgraph-practice/practice01-openai.py
@@ -24,7 +24,7 @@ completion = client.chat.completions.create(
     messages=[
         {"role": "system", "content": "You are a helpful AI assistant."},
         {"role": "user", "content": "Create a short story about a chicken and a cat. Just create the story. No need to provide the analysis or explanation."}
-        ],
+    ],
 )
 
 print(completion.choices[0].message.content)
