42ce9a97ea054591c5eb9f99999122bcb7bae3ef
asantiola <asantiola@gmail.com>
2026-06-24 22:54:00 +0800

    adjust prompt

--- src/mlx-practice/practice05-images.py
+++ src/mlx-practice/practice05-images.py
@@ -1,15 +1,22 @@
-from mlx_vlm import load, generate
+from mlx_vlm import load, stream_generate
 from mlx_vlm.prompt_utils import apply_chat_template
+import sys
 
 model_path = "mlx-community/gemma-4-12B-it-qat-6bit"
 model, processor = load(model_path)
 
 image_path = "./data/images/handwriting.jpg"
 
+prompt = """Give a brief description of what you see, then do the following:
+- If you see text, print what you see. If it is not in English, translate it.
+- If it is a puzzle, approach problems step-by-step, verify boundary conditions, 
+and rigorously check your assumptions before calculating the final answer.
+"""
+
 messages = [
     {
         "role": "user",
-        "content": "Describe what you see in the image file."
+        "content": prompt,
     }
 ]
 
@@ -20,12 +27,13 @@ formatted_prompt = apply_chat_template(
     num_images=1
 )
 
-output = generate(
+for chunk in stream_generate(
     model, 
     processor, 
     formatted_prompt,
     image=image_path,
-    temperature=0.0      
-)
-
-print(output.text)
+    temperature=0.0,
+):
+    sys.stdout.write(chunk.text)
+    sys.stdout.flush()
+print("\n\n")
